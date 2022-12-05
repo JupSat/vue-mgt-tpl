37c95fad37b88aed445e2559c8ccc5f28476a38d
JupSat <jupsat@163.com>
2022-12-05 13:29:40 +0800

    feat: 增加gzip压缩

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
+// 使用gzip压缩
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+// 定义压缩文件类型
+const productionGzipExtensions = ['js', 'css']
+
 module.exports = defineConfig({
   publicPath: './',
   assetsDir: 'static',
@@ -39,5 +44,16 @@ module.exports = defineConfig({
       // 移除 prefetch 插件
       config.plugins.delete('prefetch')
     }
+  },
+  configureWebpack: {
+    plugins: [
+      new CompressionWebpackPlugin({
+        filename: '[path][base].gz',
+        algorithm: 'gzip',
+        test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'), // 匹配文件名
+        threshold: 10240, // 对超过10K的数据进行压缩
+        minRatio: 0.8 // 极小比
+      })
+    ]
   }
 })
