3c526e32946917c0d00cdae6aca9ff34789e4295
JupSat <jupsat@163.com>
2023-02-08 22:18:11 +0800

    feat：SKU接口添加与联调

--- src/api/purchase/sku/index.js
+++ src/api/purchase/sku/index.js
@@ -5,9 +5,9 @@
  * @email: jupsat@163.com
  * @Date: 2023-02-04 18:26:13
  * @LastEditors: JupSat
- * @LastEditTime: 2023-02-04 18:27:29
+ * @LastEditTime: 2023-02-08 22:14:17
  */
-import { rPost } from '@/utils/http/function'
+import { rPost, rPostByParams } from '@/utils/http/function'
 
 const prefix = 'api'
 
@@ -15,22 +15,22 @@ const prefix = 'api'
  * @description 获取Sku信息
  * @return
  */
-export const getSkuInfo = (data) => rPost(`/${prefix}/purchase/getSkuInfo`, data)
+export const getSkuInfo = (data) => rPost(`/${prefix}/skuInfo/find`, data)
 
 /**
  * @description 添加Sku信息
  * @return
  */
-export const addSkuInfo = (data) => rPost(`/${prefix}/purchase/addSkuInfo`, data)
+export const addSkuInfo = (data) => rPost(`/${prefix}/skuInfo/add`, data)
 
 /**
  * @description 修改Sku信息
  * @return
  */
-export const editSkuInfo = (data) => rPost(`/${prefix}/purchase/editSkuInfo`, data)
+export const editSkuInfo = (data) => rPost(`/${prefix}/skuInfo/update`, data)
 
 /**
  * @description 删除Sku信息
  * @return
  */
-export const delSkuInfo = (id) => rPost(`/${prefix}/purchase/deleteSkuInfo`, id)
+export const delSkuInfo = (data) => rPostByParams(`/${prefix}/skuInfo/delete`, data)
