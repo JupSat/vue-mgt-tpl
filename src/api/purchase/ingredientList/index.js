08cf39bd97fda60261065c1a2ef0eae913225f5a
JupSat <jupsat@163.com>
2023-02-11 10:48:58 +0800

    feat：采购相关接口前缀优化

--- src/api/purchase/ingredientList/index.js
+++ src/api/purchase/ingredientList/index.js
@@ -5,32 +5,32 @@
  * @email: jupsat@163.com
  * @Date: 2023-02-08 18:03:20
  * @LastEditors: JupSat
- * @LastEditTime: 2023-02-09 16:13:27
+ * @LastEditTime: 2023-02-11 10:42:54
  */
 import { rPost } from '@/utils/http/function'
 
-const prefix = 'api'
+const prefix = 'api/ingredient'
 
 /**
  * @description 获取食材
  * @return
  */
-export const getIngredientList = (data) => rPost(`/${prefix}/ingredients/find`, data)
+export const getIngredientList = (data) => rPost(`/${prefix}/find`, data)
 
 /**
  * @description 添加食材
  * @return
  */
-export const addIngredient = (data) => rPost(`/${prefix}/ingredients/add`, data)
+export const addIngredient = (data) => rPost(`/${prefix}/add`, data)
 
 /**
  * @description 修改食材
  * @return
  */
-export const editIngredient = (data) => rPost(`/${prefix}/ingredients/update`, data)
+export const editIngredient = (data) => rPost(`/${prefix}/update`, data)
 
 /**
  * @description 删除食材
  * @return
  */
-export const delIngredient = (params) => rPost(`/${prefix}/ingredients/delete`, null, params)
+export const delIngredient = (params) => rPost(`/${prefix}/delete`, null, params)
--- src/api/purchase/purchaser/index.js
+++ src/api/purchase/purchaser/index.js
@@ -5,20 +5,20 @@
  * @email: jupsat@163.com
  * @Date: 2023-02-10 16:20:00
  * @LastEditors: JupSat
- * @LastEditTime: 2023-02-10 16:22:39
+ * @LastEditTime: 2023-02-11 10:46:36
  */
 import { rPost, rGet } from '@/utils/http/function'
 
-const prefix = 'api'
+const prefix = 'api/purchaserInfo'
 
 /**
  * @description 获取所有采购人信息
  * @return
  */
-export const getPurchasers = (data) => rGet(`/${prefix}/purchaserInfo/findAll`, data)
+export const getPurchasers = (data) => rGet(`/${prefix}/findAll`, data)
 
 /**
  * @description 添加采购人信息
  * @return
  */
-export const addPurchaser = (data) => rPost(`/${prefix}/purchaserInfo/add`, data)
+export const addPurchaser = (data) => rPost(`/${prefix}/add`, data)
