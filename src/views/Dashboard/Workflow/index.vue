986400301327efcbd85965c73afda4f9b54fae2b
JupSat <jupsat@163.com>
2023-05-24 20:48:56 +0800

    feat: websock使用

--- src/views/Dashboard/Workflow/index.vue
+++ src/views/Dashboard/Workflow/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>This is Workflow</div>
+  <div>
+    This is Workflow
+    <el-button @click="sendMsg">发送</el-button>
+  </div>
 </template>
 
 <script>
@@ -7,5 +10,15 @@ export default {
   name: 'Workflow'
 }
 </script>
-<script setup></script>
+<script setup>
+import { getCurrentInstance } from 'vue'
+const { appContext } = getCurrentInstance()
+const { $websocket } = appContext.config.globalProperties
+
+$websocket.connect()
+
+const sendMsg = () => {
+  $websocket.send()
+}
+</script>
 <style scoped lang="scss"></style>
