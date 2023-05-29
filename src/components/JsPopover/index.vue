2a511443c71ee2279ccae5105c6757cd65de2e02
JupSat <jupsat@163.com>
2023-05-29 22:12:11 +0800

    feat: 例子-文字hover时展示图片

---
+++ src/components/JsPopover/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-popover trigger="hover" placement="top" width="auto">
+    <template #default>
+      <div>
+        <el-image style="height: 120px" :src="src" :fit="'contain'" />
+      </div>
+    </template>
+    <template #reference>
+      <el-button type="text" class="underline">{{ text }}</el-button>
+    </template>
+  </el-popover>
+</template>
+
+<script>
+export default {
+  name: 'JsPopover'
+}
+</script>
+
+<script setup>
+import { defineProps } from 'vue'
+defineProps({
+  src: {
+    type: String,
+    default: ''
+  },
+  text: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.underline {
+  text-decoration: underline;
+}
+</style>
--- src/views/Dashboard/Workplace/index.vue
+++ src/views/Dashboard/Workplace/index.vue
@@ -1,11 +1,63 @@
 <template>
-  <div>This is Workplace</div>
+  <el-table :data="tableData" style="width: 100%" :row-class-name="tableRowClassName">
+    <el-table-column :prop="date" label="Date" width="180">
+      <template #default="scope">
+        <JsPopover
+          :src="'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'"
+          :text="scope.row.date"
+        ></JsPopover>
+      </template>
+    </el-table-column>
+    <el-table-column prop="name" label="Name" width="180" />
+    <el-table-column prop="address" label="Address" />
+  </el-table>
 </template>
-
 <script>
 export default {
   name: 'Workplace'
 }
 </script>
-<script setup></script>
-<style scoped lang="scss"></style>
+<script setup>
+import JsPopover from '@/components/JsPopover'
+
+const tableRowClassName = ({ row, rowIndex }) => {
+  if (rowIndex === 1) {
+    return 'warning-row'
+  } else if (rowIndex === 3) {
+    return 'success-row'
+  }
+  return ''
+}
+
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  }
+]
+</script>
+
+<style scoped lang="scss">
+.el-table .warning-row {
+  --el-table-tr-bg-color: var(--el-color-warning-light-9);
+}
+.el-table .success-row {
+  --el-table-tr-bg-color: var(--el-color-success-light-9);
+}
+</style>
