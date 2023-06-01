1f6509dfe910dd1bb41ed3b2453b9676df1e7b68
JupSat <jupsat@163.com>
2023-06-01 12:22:19 +0800

    feat: 增加权限判断方法

---
+++ src/views/Auth/RoleAuth/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column label="Date" width="180">
+      <template #default="scope">
+        <div style="display: flex; align-items: center">
+          <el-icon><timer /></el-icon>
+          <span style="margin-left: 10px">{{ scope.row.date }}</span>
+        </div>
+      </template>
+    </el-table-column>
+    <el-table-column label="Name" width="180">
+      <template #default="scope">
+        <el-popover effect="light" trigger="hover" placement="top" width="auto">
+          <template #default>
+            <div>name: {{ scope.row.name }}</div>
+            <div>address: {{ scope.row.address }}</div>
+          </template>
+          <template #reference>
+            <el-tag>{{ scope.row.name }}</el-tag>
+          </template>
+        </el-popover>
+      </template>
+    </el-table-column>
+    <el-table-column label="Operations">
+      <template #default="scope">
+        <el-button size="small" @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
+        <el-button size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+export default {
+  name: 'RoleAuth'
+}
+</script>
+<script setup>
+import { Timer } from '@element-plus/icons-vue'
+
+const handleEdit = (index, row) => {
+  console.log(index, row)
+}
+const handleDelete = (index, row) => {
+  console.log(index, row)
+}
+
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  }
+]
+</script>
+<style scoped lang="scss"></style>
